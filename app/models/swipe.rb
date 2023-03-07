8e55a72d93f7fca9999252107f9c7ccb6aa6d1ec
Matt Revell <mprevell97@gmail.com>
2023-03-07 16:13:44 +0000

    finalised basic skeleton where users can only see the matches they have and now directly link to the chat created when matched.

--- app/models/swipe.rb
+++ app/models/swipe.rb
@@ -1,4 +1,5 @@
 class Swipe < ApplicationRecord
   belongs_to :swiper, foreign_key: 'swiper_id', class_name: 'User'
   belongs_to :swipee, foreign_key: 'swipee_id', class_name: 'User'
+  has_many :matches
 end
