915547c65969d7ceebcc26495d72becb8e998a4a
Angus McCurdie <angusmccurdie1@gmail.com>
2023-03-09 16:01:33 +0000

    x

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, photos:[]])
+  end
 end
