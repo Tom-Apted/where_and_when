02bcfc7ea579ab67349ccc9863c4858613c5d9a7
SamTreon <stat123@hotmail.co.uk>
2023-03-14 15:33:46 +0000

    added photos to our date locations

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -12,23 +12,16 @@ class ChatroomsController < ApplicationController
     @match = Match.find(@chatroom.match_id)
     @suggested_date = SuggestedDate.new
     @our_dates = SuggestedDate.where(match_id: @match.id)
-
   end
 
   def multi
-    time = ["21:00"]
-    date = ["Fri 4th March"]
-    chatroom = Chatroom.find(params[:id])
-    location = DateLocation.all
-    user = current_user
     @multi_dates = []
-    # @suggested_date.match_id = chatroom.match.id
-    # @suggested_date.date_location_id = DateLocation.all.sample.id
-    # @suggested_date.user = current_user
-    6.times do
-      single_date = SuggestedDate.new(time: time.sample, date: date.sample, match_id: chatroom.match.id, date_location_id: location.sample.id, status: "proposed", user_id: user.id)
-      @multi_dates << single_date
-    end
+    outdoors_date = SuggestedDate.new(time: "5pm", date: "Sun 2nd Apr", match_id: chatroom.match.id, date_location_id: 5, status: "proposed", user_id: user.id)
+    cocktails_date = SuggestedDate.new(time: "8pm", date: "Thurs 24th Mar", match_id: chatroom.match.id, date_location_id: 8, status: "proposed", user_id: user.id)
+    coffee_date = SuggestedDate.new(time: "10am", date: "Sat 1st Apr", match_id: chatroom.match.id, date_location_id: 2, status: "proposed", user_id: user.id)
+    @multi_dates << outdoors_date
+    @multi_dates << cocktails_date
+    @multi_dates << coffee_date
   end
 
   private
--- app/models/date_location.rb
+++ app/models/date_location.rb
@@ -1,3 +1,4 @@
 class DateLocation < ApplicationRecord
   has_many :suggested_dates
+  has_one_attached :photo
 end
