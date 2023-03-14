0fd2880819bb138d6c6e59e82fcf94504f343239
tom apted <tapted0@gmail.com>
2023-03-14 15:37:58 +0000

    will no longer error

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -16,6 +16,8 @@ class ChatroomsController < ApplicationController
 
   def multi
     @multi_dates = []
+    chatroom = Chatroom.find(params[:id])
+    user = current_user
     outdoors_date = SuggestedDate.new(time: "5pm", date: "Sun 2nd Apr", match_id: chatroom.match.id, date_location_id: 5, status: "proposed", user_id: user.id)
     cocktails_date = SuggestedDate.new(time: "8pm", date: "Thurs 24th Mar", match_id: chatroom.match.id, date_location_id: 8, status: "proposed", user_id: user.id)
     coffee_date = SuggestedDate.new(time: "10am", date: "Sat 1st Apr", match_id: chatroom.match.id, date_location_id: 2, status: "proposed", user_id: user.id)
