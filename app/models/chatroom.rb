70588d66dde2415bf772a95acdb5265fe89f692c
Matt Revell <mprevell97@gmail.com>
2023-03-15 18:14:49 +0000

    ordering messages by last one sent - did this through matches controller, however still having issues on my machine with the seed file/cloudinary

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,4 +1,9 @@
 class Chatroom < ApplicationRecord
   has_many :messages
   belongs_to :match
+
+#   def latest_activity
+#     latest_message = match.chatroom.messages.last.content .order(created_at: :desc).first
+#     latest_message ? latest_message.created_at : created_at
+#   end
 end
